53c8d4d75d0481e4eb655dd81d64d2faa8741f5a
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-05-02 09:14:17 +0530

    Two Strings

---
+++ twoStrings.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the twoStrings function below.
+function twoStrings(s1, s2) {
+    for (var i = 0; i < s1.length; i++){
+        for (var j = 0; j < s2.length; j++){
+            if (s1[i] == s2[j]) {
+                return "YES";
+            }
+        }
+    }
+    return "NO";
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s1 = readLine();
+
+        const s2 = readLine();
+
+        let result = twoStrings(s1, s2);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
+
